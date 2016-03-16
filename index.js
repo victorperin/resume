4b84038ae9a0f86b0d714bbaeca0a9680086ceb5
Victor Perin <me@victorperin.com>
2016-03-15 23:51:47 -0300

    fix(generator): fix resume.html to index.html

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ var eloquent = require('jsonresume-theme-eloquent');
 var resumeJSON = JSON.parse(fs.readFileSync(path.join(__dirname, '/resume.json'), 'utf-8'));
 var resumeHTML = eloquent.render(resumeJSON);
 
-fs.writeFile(path.join(__dirname, '/resume.html'), resumeHTML, function(err) {
-    console.log(err || 'Resume exported to resume.html');
+fs.writeFile(path.join(__dirname, '/index.html'), resumeHTML, function(err) {
+    console.log(err || 'Resume exported to index.html');
 });
