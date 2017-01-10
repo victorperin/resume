9ad273536be8baa8f34dc1bace0c0ddc17580f25
Victor Perin <me@victorperin.ninja>
2017-01-09 22:10:23 -0200

    style(generator): input file on constant to improve readability

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const fs = require('fs');
 const path = require('path');
 const eloquent = require('jsonresume-theme-eloquent');
 
-const resumeJSON = JSON.parse(fs.readFileSync(path.join(__dirname, '/resume.json'), 'utf-8'));
+const inputFile = path.join(__dirname, '/resume.json');
+const resumeJSON = JSON.parse(fs.readFileSync(inputFile, 'utf-8'));
 const resumeHTML = eloquent.render(resumeJSON);
 
-fs.writeFile(path.join(__dirname, '/index.html'), resumeHTML, (err) => {
+const outputFile = path.join(__dirname, '/index.html');
+fs.writeFile(outputFile, resumeHTML, (err) => {
     console.log(err || 'Resume exported to index.html');
 });
