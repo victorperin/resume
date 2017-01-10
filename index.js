4e108f5ed1839be0efbed4de6aaaca19b24a4d08
Victor Perin <me@victorperin.ninja>
2017-01-09 22:09:01 -0200

    style(generator): arrow functions

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ const eloquent = require('jsonresume-theme-eloquent');
 const resumeJSON = JSON.parse(fs.readFileSync(path.join(__dirname, '/resume.json'), 'utf-8'));
 const resumeHTML = eloquent.render(resumeJSON);
 
-fs.writeFile(path.join(__dirname, '/index.html'), resumeHTML, function(err) {
+fs.writeFile(path.join(__dirname, '/index.html'), resumeHTML, (err) => {
     console.log(err || 'Resume exported to index.html');
 });
