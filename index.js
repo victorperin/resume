0481190aedd0bb9f9b8df264dda8a641c6e6ead9
Victor Perin <me@victorperin.ninja>
2017-01-09 22:13:45 -0200

    style(generator): organizing variables to improve readability

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ const path = require('path');
 const eloquent = require('jsonresume-theme-eloquent');
 
 const inputFile = path.join(__dirname, '/resume.json');
+const outputFile = path.join(__dirname, '/index.html');
+
 const resumeJSON = JSON.parse(fs.readFileSync(inputFile, 'utf-8'));
 const resumeHTML = eloquent.render(resumeJSON);
 
-const outputFile = path.join(__dirname, '/index.html');
 fs.writeFile(outputFile, resumeHTML, (err) => {
     console.log(err || 'Resume exported to index.html');
 });
